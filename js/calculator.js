cbb2e25f5afc6e74f7e7a71f471d7bf098c840be
weam whaeed <80516034+weam-waheed2@users.noreply.github.com>
2021-05-04 01:00:51 +0300

    Add files via upload

---
+++ js/calculator.js
@@ -0,0 +1,106 @@
+
+// Variables
+var input = document.getElementById("input")
+var number = document.querySelectorAll(".numbers div") 
+var operator = document.querySelectorAll(".operators div") 
+var result = document.getElementById("result") 
+var clear = document.getElementById("clear") 
+resultDisplayed = false; 
+
+
+// number buttons
+for (var i = 0; i < number.length; i++) {
+    number[i].addEventListener("click", function (e) {
+        var currentString = input.innerHTML;
+        var lastChar = currentString[currentString.length - 1];
+        if (resultDisplayed === false) {
+            input.innerHTML += e.target.innerHTML;
+        } 
+        else if (resultDisplayed === true && lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷") {
+            resultDisplayed = false;
+            input.innerHTML += e.target.innerHTML;
+        } 
+        else {
+            resultDisplayed = false;
+            input.innerHTML = "";
+            input.innerHTML += e.target.innerHTML;
+        }
+
+    });
+}
+
+
+
+// click number buttons
+for (var i = 0; i < operator.length; i++) {
+    operator[i].addEventListener("click", function (e) {
+        var currentString = input.innerHTML;
+        var lastChar = currentString[currentString.length - 1];
+        if (lastChar === "+" || lastChar === "-" || lastChar === "×" || lastChar === "÷") {
+            var newString = currentString.substring(0, currentString.length - 1) + e.target.innerHTML;
+            input.innerHTML = newString;
+        } 
+        else if (currentString.length == 0) {
+            console.log("enter a number first");
+        } 
+        else {
+            input.innerHTML += e.target.innerHTML;
+        }
+
+    });
+}
+
+
+
+// equal button
+result.addEventListener("click", function () {
+    var inputString = input.innerHTML;
+    var numbers = inputString.split(/\+|\-|\×|\÷/g);
+    var operators = inputString.replace(/[0-9]|\./g, "").split("");
+
+    console.log(inputString);
+    console.log(operators);
+    console.log(numbers);
+    console.log("----------------------------");
+
+
+
+    var divide = operators.indexOf("÷");
+    while (divide != -1) {
+        numbers.splice(divide, 2, numbers[divide] / numbers[divide + 1]);
+        operators.splice(divide, 1);
+        divide = operators.indexOf("÷");
+    }
+
+    var multiply = operators.indexOf("×");
+    while (multiply != -1) {
+        numbers.splice(multiply, 2, numbers[multiply] * numbers[multiply + 1]);
+        operators.splice(multiply, 1);
+        multiply = operators.indexOf("×");
+    }
+
+    var subtract = operators.indexOf("-");
+    while (subtract != -1) {
+        numbers.splice(subtract, 2, numbers[subtract] - numbers[subtract + 1]);
+        operators.splice(subtract, 1);
+        subtract = operators.indexOf("-");
+    }
+
+    var add = operators.indexOf("+");
+    while (add != -1) {
+        numbers.splice(add, 2, parseFloat(numbers[add]) + parseFloat(numbers[add + 1]));
+        operators.splice(add, 1);
+        add = operators.indexOf("+");
+    }
+
+    input.innerHTML = numbers[0];
+
+    resultDisplayed = true;
+});
+
+
+
+// clear the input
+clear.addEventListener("click", function () {
+    input.innerHTML = "";
+})
